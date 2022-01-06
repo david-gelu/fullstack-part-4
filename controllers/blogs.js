7cc630791da589dce797be2f29c517f014d9a09e
david-gelu <david.gelu90@gmail.com>
2022-01-06 16:57:58 +0200

    feat:use async await unstead promises

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,13 +1,11 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then(blogs => {
-    response.json(blogs)
-  })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
-
-blogsRouter.post('/', (request, response, next) => {
+blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
   if (body.title === undefined) {
@@ -23,21 +21,15 @@ blogsRouter.post('/', (request, response, next) => {
     url: body.url,
     likes: body.likes
   })
-
-  blog
-    .save()
-    .then(saveBlog => response.json(saveBlog))
-    .then(savedAndFormattedBlog => {
-      response.json(savedAndFormattedBlog)
-    })
-    .catch(error => next(error))
+  const savedBlog = await blog.save()
+  response.json(savedBlog)
 })
 
-blogsRouter.get('/:id', (request, response, next) => {
-  Blog.findById(request.params.id)
-    .then(blog => {
-      if (blog) {
-        response.send(`
+blogsRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  if (blog) {
+    response.send(
+      `
         {
         <pre>
         <strong>id:</strong> <span style="color:green; font-weight:900;">${request.params.id}</span>
@@ -46,20 +38,18 @@ blogsRouter.get('/:id', (request, response, next) => {
         <strong>url:</strong> <span style="color:green; font-weight:900;">${blog.url}</span>
         <strong>likes:</strong><span style="color:green; font-weight:900;"> ${blog.likes}</span>
         </pre>
-        }
-        `)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
+        }`
+    )
+  } else {
+    response.status(404).end()
+  }
 })
 
-blogsRouter.put('/:id', (request, response, next) => {
+blogsRouter.put('/:id', async (request, response, next) => {
   const paramBody = request.params
   const reqBody = request.body
   const resBody = response.req.body
-  Blog.findByIdAndUpdate(paramBody.id, { $set: { author: resBody.author } }, { new: true },
+  await Blog.findByIdAndUpdate(paramBody.id, { $set: { author: resBody.author } }, { new: true },
     (error, data) => {
       if (error) error => next(error)
       else response.status(200).send(data)
@@ -67,17 +57,15 @@ blogsRouter.put('/:id', (request, response, next) => {
     })
 })
 
-blogsRouter.delete('/:id', (request, response, next) => {
+blogsRouter.delete('/:id', async (request, response) => {
   const body = request.params
-  Blog.findByIdAndRemove(body.id)
-    .then(() => {
-      response.status(200).end()
-    })
-    .catch(error => next(error))
+  await Blog.findByIdAndRemove(body.id)
+  response.status(200).end()
 })
-blogsRouter.get('/info', (request, response) => {
+
+blogsRouter.get('/api/info', (request, response) => {
   response.send(`
-  <h2>Blog has info for ${length} posts</h2>
+  <h2>Blog has info for ${Blog.length - 1} posts</h2>
   <h3>${new Date()}<h3>
   `)
 })
